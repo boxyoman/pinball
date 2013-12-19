0e734732e51d269d0b87af075083a43802ec7551
rbrague1480 <rbrague1480@gmail.com>
2013-12-18 23:13:55 -0500

    changed pin numbers

--- outputs.h
+++ outputs.h
@@ -7,50 +7,50 @@
 class pOutBumper1 : public pOutput{
 	pGame &game;
 	public:
-		pOutBumper1(pGame &g) : game(g), pOutput(48, 70, 0, LOW){};
+		pOutBumper1(pGame &g) : game(g), pOutput(47, 70, 0, LOW){};
 };
 
 class pOutBumper2 : public pOutput{
 	pGame &game;
 	public:
-		pOutBumper2(pGame &g) : game(g), pOutput(50, 70, 0, LOW){};
+		pOutBumper2(pGame &g) : game(g), pOutput(49, 70, 0, LOW){};
 };
 
 class pOutBumper3 : public pOutput{
 	pGame &game;
 	public:
-		pOutBumper3(pGame &g) : game(g), pOutput(46, 70, 0, LOW){};
+		pOutBumper3(pGame &g) : game(g), pOutput(45, 70, 0, LOW){};
 };
 
 class pOutDropTargets : public pOutput{
 	public:
 		pGame &game;
-		pOutDropTargets(pGame &g) : game(g), pOutput(44, 70, 500, LOW){};
+		pOutDropTargets(pGame &g) : game(g), pOutput(53, 70, 500, LOW){};
 		void onActive();
 };
 
 class pOutFlickUp : public pOutput{
 	pGame &game;
 	public:
-		pOutFlickUp(pGame &g) : game(g), pOutput(52, 70, 1000, LOW){};
+		pOutFlickUp(pGame &g) : game(g), pOutput(39, 70, 1000, LOW){};
 };
 
 class pOutLowBumperLeft : public pOutput{
 	pGame &game;
 	public:
-		pOutLowBumperLeft(pGame &g) : game(g), pOutput(30, 70, 0, LOW){};
+		pOutLowBumperLeft(pGame &g) : game(g), pOutput(51, 70, 0, LOW){};
 };
 
 class pOutLowBumperRight : public pOutput{
 	pGame &game;
 	public:
-		pOutLowBumperRight(pGame &g) : game(g), pOutput(32, 70, 0, LOW){};
+		pOutLowBumperRight(pGame &g) : game(g), pOutput(41, 70, 0, LOW){};
 };
 
 class pOutReturnBall : public pOutput{
 	pGame &game;
 	public:
-		pOutReturnBall(pGame &g) : game(g), pOutput(42, 70, 1000, LOW){};
+		pOutReturnBall(pGame &g) : game(g), pOutput(43, 70, 1000, LOW){};
 };
 
 
